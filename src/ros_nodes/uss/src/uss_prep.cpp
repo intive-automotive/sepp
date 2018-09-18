500a5f7d5188fa20a8511682e8743d128e36cf21
Andreas Lachenschmidt <a.lachenschmidt@web.de>
2018-09-18 17:06:18 +0200

    added file headers
    desciption of purpose of the files

--- src/ros_nodes/uss/src/uss_prep.cpp
+++ src/ros_nodes/uss/src/uss_prep.cpp
@@ -1,3 +1,33 @@
+// ROS node for SEPP - prepare inputs from ultrasonic sensors of SEPP
+// 2018-09-18 by Andreas Lachenschmidt <a.lachenschmidt@web.de>
+//
+// Changelog:
+//      2018-09-18 - initial commit
+
+/* ============================================
+SEPP ROS Nodes are placed under the MIT license
+Copyright (c) 2018 Andreas Lachenschmidt
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+===============================================
+*/
+
 #include <string.h>
 #include <sstream>
 #include <ros/ros.h>
